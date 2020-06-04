b4ccee8a75d25caf4d679fefff84f4728abad99e
Hajres Swayah <hajres.swayah@gmail.com>
2020-06-04 13:16:26 -0400

    adding master's files

---
+++ src/utils/helpers.js
@@ -0,0 +1,8 @@
+  
+export function isObject (item) {
+  return Object.prototype.toString.call(item) === '[object Object]'
+}
+
+export function getPercentage (count, total) {
+  return total === 0 ? 0 : parseInt(count / total * 100, 10)
+}
